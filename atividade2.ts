a3bd3823ea563ce33d8c0c7c8be17557b9cc6569
Ronayby <ronayby2@gmail.com>
2025-11-19 20:52:16 -0300

    Add atividade2.ts to demonstrate type usage

---
+++ atividade2.ts
@@ -0,0 +1,11 @@
+let nota: number | string;
+
+nota = 9.5;
+console.log(`Valor atual: ${nota}`);
+console.log(`Tipo atual (number): ${typeof nota}`);
+
+console.log("------------------------");
+
+nota = "Aprovado";
+console.log(`Valor atual: ${nota}`);
+console.log(`Tipo atual (string): ${typeof nota}`);
